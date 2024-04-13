103bf52dcdf123f6aa18cb825a797074d7d012e4
Alejandro Romero <alejo8591@gmail.com>
2024-04-12 22:11:51 -0500

    Module-III: Add lab9 (Partial code) use Function & Scopes in JS

---
+++ module_III/lab9/lab9.js
@@ -0,0 +1,29 @@
+/**
+ *     @author Alejandro Romero <alejo8591@gmail.com>
+ *     @fileOverview This script use Function & Scopes in JS
+ *     @licence BSD 3-Clause License
+ */
+
+// declaration vars
+let email = null;
+let firstname = null;
+let lastName = null;
+let phone = null;
+let password = null;
+let user = {};
+
+// getData
+const getDataFormMyForm = () => {
+    //1. first step load data from html
+    const floatingInputEmail = document.getElementById("floatingInputEmail").value;
+    const floatingInputName = document.getElementById("floatingInputName").value;
+    const floatingInputLastname = document.getElementById("floatingInputLastname").value;
+    const floatingInputPhone = document.getElementById("floatingInputPhone").value;
+    const floatingPassword = document.getElementById("floatingPassword").value;
+    debugger
+};
+
+//2. sanitize data
+
+//3. prepare to send
+
